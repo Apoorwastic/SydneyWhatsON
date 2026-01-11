fc99029f375a14080f507008df1005a1a751f916
Apoorwastic <77602734+Apoorwastic@users.noreply.github.com>
2026-01-12 01:17:22 +0530

    Update events.js

--- frontend/src/api/events.js
+++ frontend/src/api/events.js
@@ -1,5 +1,8 @@
+const API_BASE =
+  import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
+
 export const fetchEvents = async () => {
   const res = await fetch(`${API_BASE}/events`);
   const data = await res.json();
-  return data.events; // ✅ return ARRAY only
+  return data.events; // ✅ IMPORTANT
 };
